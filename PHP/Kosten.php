086aa06cb80c908f97fd092926c3d0a3954a19d0
JordanMaarten <124160@student.drenthecollege.nl>
2019-02-11 15:05:24 +0100

    added case,moederboord,storage,case and index.

---
+++ PHP/Kosten.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<html lang="en" dir="ltr">
+
+  <head>
+
+    <meta charset="utf-8">
+
+    <title></title>
+
+  </head>
+
+  <body>
+
+    <h1> Hoeveel gaat het kosten? </h1>
+
+    <?php
+
+     ?>
+
+  </body>
+
+</html>
